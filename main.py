60a36c3f574c07c9abf9a065038fb42a1edcfdd7
thisisignitedoreo <thisisignitedoreo@gmail.com>
2022-09-16 19:56:42 +0300

    RatLauncher: RL 2.4
    Что нового:
    - Темная/светлая тема
    - Стандартная тема Fusion
    - News Reader

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ import shutil
 import os
 import requests
 import json
-import zipfile
+from requests_html import HTMLSession
+from bs4 import BeautifulSoup
 
 app = QtWidgets.QApplication(sys.argv)
 form = QtWidgets.QWidget()
@@ -18,20 +19,62 @@ ui = Ui_Form()
 ui.setupUi(form)
 form.show()
 
+htmlsession = HTMLSession()
+
+app.setStyle("Fusion")
+
+white_palette = QtGui.QPalette()
+dark_palette = QtGui.QPalette()
+
+dark_palette.setColor(QtGui.QPalette.Window, QtGui.QColor(53, 53, 53))
+dark_palette.setColor(QtGui.QPalette.WindowText, QtGui.Qt.white)
+dark_palette.setColor(QtGui.QPalette.Base, QtGui.QColor(25, 25, 25))
+dark_palette.setColor(QtGui.QPalette.AlternateBase, QtGui.QColor(53, 53, 53))
+dark_palette.setColor(QtGui.QPalette.ToolTipBase, QtGui.Qt.white)
+dark_palette.setColor(QtGui.QPalette.ToolTipText, QtGui.Qt.white)
+dark_palette.setColor(QtGui.QPalette.Text, QtGui.Qt.white)
+dark_palette.setColor(QtGui.QPalette.Button, QtGui.QColor(53, 53, 53))
+dark_palette.setColor(QtGui.QPalette.ButtonText, QtGui.Qt.white)
+dark_palette.setColor(QtGui.QPalette.BrightText, QtGui.Qt.red)
+dark_palette.setColor(QtGui.QPalette.Link, QtGui.QColor(42, 130, 218))
+dark_palette.setColor(QtGui.QPalette.Highlight, QtGui.QColor(42, 130, 218))
+dark_palette.setColor(QtGui.QPalette.HighlightedText, QtGui.Qt.black)
+
 minecraft_folder = "gamefiles"
 
 if not os.path.isfile("accounts.json"):
     open("accounts.json", "w").write(f"[\"RatUser_{rint(100000, 999999)}\"]")
 
+if not os.path.isfile("settings.json"):
+    open("settings.json", "w").write(json.dumps({
+        "theme": "light"
+    }))
+
 if os.path.isdir(minecraft_folder):
     open(minecraft_folder + "/launcher_profiles.json", "w").write("{}")
 
 with open("accounts.json") as acc:
-    accounts = json.loads(acc.read())
+    accounts = json.load(acc)
 
-if accounts == []:
+with open("settings.json") as settings:
+    settings = json.load(settings)
+
+if not accounts:
     accounts = ["RatUser_" + str(rint(100000, 999999))]
+if not settings:
+    accounts = {"theme": "white"}
 
+match settings["theme"]:
+    case "light":
+        qApp.setPalette(white_palette)
+        app.setStyleSheet("")
+        ui.radioButton_5.setChecked(True)
+        ui.radioButton_6.setChecked(False)
+    case "dark":
+        qApp.setPalette(dark_palette)
+        qApp.setStyleSheet("QToolTip { color: #ffffff; background-color: #2a82da; border: 1px solid white; }")
+        ui.radioButton_5.setChecked(False)
+        ui.radioButton_6.setChecked(True)
 current_max = 0
 
 
@@ -240,6 +283,49 @@ def customRes():
     ui.lineEdit.setEnabled(ui.checkBox_4.isChecked())
     ui.lineEdit_3.setEnabled(ui.checkBox_4.isChecked())
 
+def setTheme(isDark):
+    if isDark:
+        qApp.setPalette(dark_palette)
+        qApp.setStyleSheet("QToolTip { color: #ffffff; background-color: #2a82da; border: 1px solid white; }")
+        settings["theme"] = "dark"
+    else:
+        qApp.setPalette(white_palette)
+        app.setStyleSheet("")
+        settings["theme"] = "white"
+    with open("settings.json", "w") as file:
+        file.write(json.dumps(settings))
+
+news = {}
+newslst = {}
+
+def updateNews():
+    global news, newslst
+    news = mll.utils.get_minecraft_news(-1)
+    news = mll.utils.get_minecraft_news(news["article_count"])
+    ui.label_5.setText(f"Всего новостей: {news['article_count']}")
+    ui.listWidget.clear()
+    newslst = {}
+    for k, i in enumerate(news["article_grid"]):
+        ui.listWidget.addItem(i["default_tile"]["title"])
+        newslst.update({i["default_tile"]["title"]: k})
+
+def loadNews(item):
+    for i in os.listdir("temp"):
+        os.remove(f"temp/{i}")
+    article = news["article_grid"][newslst[item.text()]]
+    html = htmlsession.get("https://www.minecraft.net" + article["article_url"]).text
+    soup = BeautifulSoup(html, "html.parser")
+    for i in [f'https://minecraft.net{i.get("src")}' for i in soup.select('img.article-image-carousel__image')]:
+        img = htmlsession.get(i).content
+        with open(f"temp/{i.split('/')[-1:][0]}", "wb") as file:
+            file.write(img)
+    ui.textEdit.setHtml(f"""<h1>{soup.select('div.page-section:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > h1:nth-child(1)')[0]}</h1>
+<h2>{soup.select('.lead')[0]}</h2>
+{''.join([f'''<img width="240" height="160" src="temp/{i.get("src").split("/")[-1:][0]}" /><br>
+{i.get("alt")}''' for i in soup.select('img.article-image-carousel__image')])}
+<p>{'</p><p>'.join([str(i) for i in soup.select('.end-with-block')])}</p>
+""")
+
 fetchVersions()
 fetchAccounts()
 
@@ -250,5 +336,11 @@ ui.radioButton_4.toggled.connect(impactActivated)
 ui.toolButton_4.clicked.connect(addAccount)
 ui.toolButton_2.clicked.connect(popAccount)
 ui.checkBox_4.toggled.connect(customRes)
+ui.radioButton_6.toggled.connect(lambda: setTheme(True))
+ui.radioButton_5.toggled.connect(lambda: setTheme(False))
+ui.pushButton_3.clicked.connect(lambda: ui.stackedWidget.setCurrentIndex(1))
+ui.toolButton_3.clicked.connect(lambda: ui.stackedWidget.setCurrentIndex(0))
+ui.toolButton_5.clicked.connect(updateNews)
+ui.listWidget.itemDoubleClicked.connect(loadNews)
 
 sys.exit(app.exec_())
